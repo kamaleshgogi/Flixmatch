c40b85f5c917e57e4953fb79860374d4c0e0a5fe
kamaleshgogi <gogikamalesh@gmail.com>
2025-02-13 14:00:13 -0700

    updates index

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'frontend', // Set the root to the 'frontend' directory
+  root: '.', // Ensure the root is set to the current directory
   build: {
-    outDir: '../dist', // Adjust the output directory if needed
+    outDir: 'dist', // Adjust the output directory if needed
   },
 })
